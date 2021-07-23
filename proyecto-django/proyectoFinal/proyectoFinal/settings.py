7555afec185e42a762bb82bd730ebc3fb3d16b1c
fjsaca2001 <sacafrank2@gmail.com>
2021-07-22 22:32:04 -0500

    Servidor Nginx

--- proyecto-django/proyectoFinal/proyectoFinal/settings.py
+++ proyecto-django/proyectoFinal/proyectoFinal/settings.py
@@ -128,14 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/proyectoFinal/static/'
-FORCE_SCRIPT_NAME = '/proyectoFinal'
+STATIC_URL = '/static/'
+#FORCE_SCRIPT_NAME = '/proyectoFinal'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGOUT_REDIRECT_URL = '/' # new
 
